bb1fabdc8e7dce072be9a2b97c41821d9aa34a60
Tecky <teckybot128@gmail.com>
2023-12-17 20:14:37 +0530

    add database setup sql file

---
+++ sql_files/schema.sql
@@ -0,0 +1,12 @@
+-- Creating the task manager database
+CREATE DATABASE IF NOT EXISTS taskmanagerdb;
+
+-- Use the database
+USE taskmanagerdb;
+
+-- Creating  the tasks table
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT
+);
